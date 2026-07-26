170932f3dbc511e1b7c49307b0de0892724a7bb4
workdisk501-art <workdisk501@gmail.com>
2026-07-26 16:44:28 +0300

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -87,9 +87,9 @@ function MaturityMap() {
           <div style={{ position: 'relative', width: '100%', marginBottom: 8 }}>
             {/* SVG trajectory arc */}
             <svg
-              viewBox="0 0 900 110"
+              viewBox="0 0 900 160"
               preserveAspectRatio="none"
-              style={{ width: '100%', height: 110, display: 'block' }}
+              style={{ width: '100%', height: 160, display: 'block' }}
             >
               {/* Dashed path */}
               <path
